05387de1b1d5ed6131cd0dcb708a3922c829aba8
Seungjun Song <gunmagician@naver.com>
2025-07-30 15:12:21 +0900

    ⚡️ : feat 3/ Pagenation ajax scraping
    뉴스 더보기 버튼 클릭시 나오는 뉴스들까지 가져오기 위해서 ajax_url추가, 3페이지까지 나옴

--- malicious_news.py
+++ malicious_news.py
@@ -1,28 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
 
-def crawl_malicious_news():
+def crawl_malicious_news(max_page=3):
     """
-    악성 웹사이트 관련 뉴스를 크롤링하여 리스트로 반환합니다.
-    각 뉴스는 {'title': 제목, 'img': 이미지URL, 'link': 링크} 형태의 딕셔너리입니다.
+    보안뉴스 모바일 '더보기' AJAX를 활용해 여러 페이지의 뉴스를 크롤링합니다.
+    max_page: 몇 페이지까지 가져올지(1=최신, 2=더보기 1번, ...)
     """
-
     base_url = "https://m.boannews.com/html/"
-    url = base_url + "Main_Menu.html?mkind=1"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    ajax_url = base_url + "Main_Menu.html"
     news_list = []
-    for li in soup.select('ul.tab-newslist li'):
-        a_tag = li.find('a', href=True)
-        if not a_tag:
-            continue
-        link = a_tag['href']
-        if not link.startswith('http'):
-            link = base_url + link
-        img_tag = a_tag.find('img')
-        img_url = img_tag['src'] if img_tag else ''
-        title_span = a_tag.find_all('span')
-        # 두 번째 span이 제목
-        title = title_span[1].get_text(strip=True) if len(title_span) > 1 else ''
-        news_list.append({'title': title, 'img': img_url, 'link': link})
+
+    for page in range(1, max_page + 1):
+        params = {"mkind": 1, "page1": page}
+        response = requests.get(ajax_url, params=params)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        for li in soup.select('ul.tab-newslist li'):
+            a_tag = li.find('a', href=True)
+            if not a_tag:
+                continue
+            link = a_tag['href']
+            if not link.startswith('http'):
+                link = base_url + link
+            img_tag = a_tag.find('img')
+            img_url = img_tag['src'] if img_tag else ''
+            title_span = a_tag.find_all('span')
+            title = title_span[1].get_text(strip=True) if len(title_span) > 1 else ''
+            news_list.append({'title': title, 'img': img_url, 'link': link})
     return news_list
