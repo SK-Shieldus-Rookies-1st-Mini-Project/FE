c24cb5d23e12a7fa8eb692aebb9e05962dc8a8ab
Seungjun Song <gunmagician@naver.com>
2025-07-30 16:15:48 +0900

    ⚡️ : feat 4/ Pagenation ajax scraping

--- malicious_news.py
+++ malicious_news.py
@@ -1,29 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
 
-def crawl_malicious_news(max_page=3):
+def crawl_malicious_news():
     """
-    보안뉴스 모바일 '더보기' AJAX를 활용해 여러 페이지의 뉴스를 크롤링합니다.
-    max_page: 몇 페이지까지 가져올지(1=최신, 2=더보기 1번, ...)
+    Main_Menu.html에서 모든 뉴스 목록을 가져오고,
+    각 뉴스의 detail 페이지에서 제목/이미지/본문을 추출합니다.
     """
     base_url = "https://m.boannews.com/html/"
-    ajax_url = base_url + "Main_Menu.html"
+    url = base_url + "Main_Menu.html"
+    params = {"page1": 1}
     news_list = []
 
-    for page in range(1, max_page + 1):
-        params = {"mkind": 1, "page1": page}
-        response = requests.get(ajax_url, params=params)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        for li in soup.select('ul.tab-newslist li'):
-            a_tag = li.find('a', href=True)
-            if not a_tag:
-                continue
-            link = a_tag['href']
-            if not link.startswith('http'):
-                link = base_url + link
-            img_tag = a_tag.find('img')
-            img_url = img_tag['src'] if img_tag else ''
-            title_span = a_tag.find_all('span')
-            title = title_span[1].get_text(strip=True) if len(title_span) > 1 else ''
-            news_list.append({'title': title, 'img': img_url, 'link': link})
+    response = requests.get(url, params=params)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for li in soup.select('ul.tab-newslist li'):
+        a_tag = li.find('a', href=True)
+        if not a_tag:
+            continue
+        href = a_tag['href']
+        if not href.startswith('http'):
+            detail_url = base_url + href
+        else:
+            detail_url = href
+        try:
+            detail_resp = requests.get(detail_url)
+            detail_soup = BeautifulSoup(detail_resp.text, 'html.parser')
+            # 제목
+            title_tag = detail_soup.select_one('div.tit > p')
+            title = title_tag.get_text(strip=True) if title_tag else ''
+            # 이미지
+            img_tag = detail_soup.find('meta', attrs={'property': 'og:image'})
+            img_url = img_tag['content'] if img_tag and img_tag.get('content') else ''
+            # 본문
+            content_tag = detail_soup.select_one('.con #con')
+            content = str(content_tag) if content_tag else ''
+        except Exception:
+            title = ''
+            img_url = ''
+            content = ''
+        if title:
+            news_list.append({'title': title, 'img': img_url, 'link': detail_url, 'content': content})
     return news_list
